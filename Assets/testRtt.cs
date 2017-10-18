eb20ca14d2204c249c26d7bc61719be4f145c03c
terry <yutianyu@gmail.com>
2017-10-18 11:17:15 +0800

    首次提交，准备好问题测试。

---
+++ Assets/testRtt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testRtt : MonoBehaviour {
+    [SerializeField]
+    Shader shader; // assign mattatz/MaskBloom shader.
+    Material material;
+    // Use this for initialization
+    void Start () {
+        material = new Material(shader);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        var mask = RenderTexture.GetTemporary(src.width, src.height, 0, src.format);
+        mask.filterMode = FilterMode.Bilinear;
+        Graphics.Blit(src, mask, material, 0); // masking
+ 
+
+        Graphics.Blit(mask, dst);
+ 
+    }
+ }
